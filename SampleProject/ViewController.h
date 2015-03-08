930fc99310549e7ba485de273e0a3dda1ba6ec80
Mary Jenel <mmyers7@hawaii.edu>
2015-03-07 18:00:12 -0800

    Initial Commit

---
+++ SampleProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleProject
+//
+//  Created by Mary Jenel Myers on 3/7/27 H.
+//  Copyright (c) 27 Heisei Mary Jenel Myers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SampleProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SampleProject
+//
+//  Created by Mary Jenel Myers on 3/7/27 H.
+//  Copyright (c) 27 Heisei Mary Jenel Myers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
