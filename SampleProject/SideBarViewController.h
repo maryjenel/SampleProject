9f8c2d58b06efd181f65dacf6721ffddf47a265e
Mary Jenel <mmyers7@hawaii.edu>
2015-03-09 15:57:59 -0700

    scrollview enabled

---
+++ SampleProject/SideBarViewController.h
@@ -0,0 +1,14 @@
+//
+//  SideBarViewController.h
+//  SampleProject
+//
+//  Created by Mary Jenel Myers on 3/9/27 H.
+//  Copyright (c) 27 Heisei Mary Jenel Myers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SideBarViewController : UIViewController
+
+-(instancetype)initWithLeftViewController:(UIViewController *)leftViewController mainViewController:(UIViewController *)mainViewController gap:(NSInteger)gap;
+@end
